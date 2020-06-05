011622844af155930fad74a68acb027cd8014546
Sergio Pachón <sap9827@gmail.com>
2020-06-05 02:38:07 -0500

    Puse una pregunta en el texto de salida

--- part_a.py
+++ part_a.py
@@ -1,7 +1,7 @@
 
 def saludo():
     persona = input('Nombre de una persona: ')
-    print(f'Hola, este es mi saludo {persona}')
+    print(f'Hola como estás?, este es mi saludo {persona}')
 
 if __name__=='__main__':
     saludar = saludo()
