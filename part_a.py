9b649df81faee852d5495565f3be58037848e6cb
Sergio Pachón <sap9827@gmail.com>
2020-06-05 01:28:54 -0500

    Una prueba de creación

---
+++ part_a.py
@@ -0,0 +1,8 @@
+
+
+
+def saludo():
+    print('Hola, este es mi saludo')
+
+if __name__=='__main__':
+    saludar = saludo()
---
+++ part_b.py
@@ -0,0 +1,7 @@
+
+def despedida():
+    print('Nospi')
+
+if __name__=='__main__':
+    despedirse = despedida()
+
